b710f277dccf30fcd36b7b7837042a8b7fd215f9
Håkon Wang Schutt <hakon@schutt.no>
2018-11-29 01:19:49 +0100

    recreated api with docker

--- 2_task1/api/Controllers/ValuesController.cs
+++ 2_task1/api/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace PoliceCaseApi.Controllers
+namespace api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
